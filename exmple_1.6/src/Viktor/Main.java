2a5d881745e3f341f257254a1bee7423b7b98e49
K.Viktor <svry88@gmail.com>
2020-11-09 01:18:27 +0300

    1

--- exmple_1.6/src/Viktor/Main.java
+++ exmple_1.6/src/Viktor/Main.java
@@ -8,20 +8,20 @@ package Viktor;
 public class Main {
 
     public static void main(String[] args) {
-	    godVisokosniy(2020);
-    	godVisokosniy(2021);
+        godVisokosniy(2020);
+        godVisokosniy(2021);
         godVisokosniy(4);
         godVisokosniy(100);
         godVisokosniy(-4900);
     }
-    static void godVisokosniy (int a) {
-        if (a>0) {
+
+    static void godVisokosniy(int a) {
+        if (a > 0) {
             if ((a % 4 == 0) && (a % 100 != 0) || (a % 400 == 0))
                 System.out.println(a + ": високосный год");
             else
                 System.out.println(a + ": не високосный год");
-        }
-        else
+        } else
             System.out.println("введите положительное число");
     }
 }
